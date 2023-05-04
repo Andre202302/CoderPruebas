f8c805fad9232a65e7d2b44cf6c0c7cbbc0da0f4
Leidy Andrea <124695673+Andre202302@users.noreply.github.com>
2023-05-03 20:29:39 -0500

    Add files via upload

--- js/carrito.js
+++ js/carrito.js
@@ -4,7 +4,7 @@ const mostrarServicios = (data) =>{
   contenedorServicios.innerHTML = ""; //para que no se vuelva a renderizar se asigna como un vacio
   data.forEach(servicio => {
     const cardService = document.createElement('article');//contenedor de cada uno de los productos de mi tienda
-    //cardService.setAttribute('id', 'tarjeta-compra');//le creo un atributo para estilos
+    cardService.setAttribute('id', 'tarjeta-compra');//le creo un atributo para estilos
     cardService.innerHTML =`
                             <img class ="ser-img" src="${servicio?.img}" alt="${servicio?.nombre}" style="width: 200px;margin-left: 100px" >
                             
@@ -18,16 +18,15 @@ const eliminar = document.getElementById(`${servicio.id}`);
 eliminar.addEventListener('click', (e) => {
 eliminarDelCarrito(e.target.id)
 });
-    ////*****total del carrito */
-    const total = carrito.reduce((acc,el) => acc + el.precio, 0);
-    const totalBuying = document.querySelector("#total-carrito");
-    totalBuying.innerHTML =  `TOTAL A PAGAR: $${total} `;
-    contenedorServicios.appendChild(totalBuying);
-    }) 
-
+   }) 
   } 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 mostrarServicios(carrito);
+////*****total del carrito */
+const total = carrito.reduce((acc,el) => acc + el.precio, 0);
+const totalBuying = document.querySelector("#total-carrito");
+totalBuying.innerHTML =  `TOTAL A PAGAR: $${total} `;
+contenedorServicios.appendChild(totalBuying);
     
    ///////GuardarLS///////////
   function guardarLS(elemento){
